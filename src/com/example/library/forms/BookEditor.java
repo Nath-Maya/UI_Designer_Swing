57132c816d485e32148cecaf34ae2fc887fdbd4d
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-07-01 15:48:34 -0500

    Main panel, add 2 text fields, drop-down list and save and cancel button.

--- src/com/example/library/forms/BookEditor.java
+++ src/com/example/library/forms/BookEditor.java
@@ -1,7 +1,14 @@
 package com.example.library.forms;
+import javax.swing.*;
 import java.swing.*;
 
 public class BookEditor {
 
     private JPanel contentPane;
+    private JTextField bookNameField;
+    private JTextField authorNameField;
+    private JComboBox genreComboBox;
+    private JCheckBox isTakenCheckBox;
+    private JButton cancelButton;
+    private JButton saveButton;
 }
